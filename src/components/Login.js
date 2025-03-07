cf5e70a7f991f83cc50a9ca9edd29f92da288951
AhsanLaeeq <arahsan125@gmail.com>
2025-03-07 21:37:02 +0500

    Initial commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -78,7 +78,7 @@ const Login = (props) => {
     e.preventDefault();
 
     // API call to authenticate user
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://backendnest-production-a25a.up.railway.app/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
